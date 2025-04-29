50fa697e6c0fa39deff32bf51f04c97a85fc166b
Sely-duart <giselyduarte114@gmail.com>
2025-04-29 08:39:47 -0300

    cerebro.js

--- cerebro.js
+++ cerebro.js
@@ -5,13 +5,22 @@ botao.addEventListener("click",function(){
 }); 
 
 function somar(){
- const num1 = 
+    const num1 = 
 parseFloat(document.getElementById("num1").value);
  const num2 = 
-parseFloat(document.getElementById("num2").value);
+parseFloat(        document.getElementById("num2").value);
  const resultado = num1 + num2; 
- document.getElementById("resultado").innerHTML = `resultado:${resultado}`; 
+    document.getElementById("resultado").innerHTML = `resultado:${resultado}`; 
 }
 
 document.getElementById("titulo").innerHTML="Novo Titulo!";
 
+function converter() {
+    const celsius = document.getElementById("celsius").value;
+    if (celsius === "") {
+        document.getElementById("resultado").innerText = "Por favor, insira um valor.";
+        return;
+    }
+    const fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("resultado").innerText = `${celsius}°C é igual a ${fahrenheit.toFixed(2)}°F.`;
+}
